6b41aed9d1ee9015c8d1ce15f5b403cf67bad694
Hamza1610 <muhammadhamza162003@gmail.com>
2024-05-06 05:24:05 -0700

    move test to root module

---
+++ data_generator/test_model.py
@@ -0,0 +1,15 @@
+from dotenv import get_key
+from model import Model
+
+api_key = get_key('.env', 'GOOGLE_API_KEY')
+
+print(api_key)
+data_generator = Model(api_key)
+
+data = data_generator.generate(
+    {'cols': ['Instructions', 'Shell commands','funtions of commands', 'secure rate'], 'rows': 100},
+    'Generate shell commands for diferent operating systems'
+    )
+
+f = open('demofile.csv', 'a')
+f.write(data)
