81a7fbffac77b14ab5465901cbd04dc8f543a399
Hamza1610 <muhammadhamza162003@gmail.com>
2024-05-09 13:01:56 -0700

    print file and Model class

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
 from dotenv import load_dotenv
+from file_manager.file import File_manager
+from data_generator.model import Model
 # load environment variables
 load_dotenv()
 
+print(File_manager, Model)
