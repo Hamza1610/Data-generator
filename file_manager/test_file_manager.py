3db5b143be592d2aa3247ec76b99ad2b7e44daa1
Hamza1610 <muhammadhamza162003@gmail.com>
2024-05-09 13:01:28 -0700

    modify test file manager

--- file_manager/test_file_manager.py
+++ file_manager/test_file_manager.py
@@ -1,11 +1,10 @@
 from file import File_manager
 
 file = File_manager()
+ 
+print(file.create('test.txt'))
 
-# print(file.create('test.txt'))
 
-# print(file.gettinfo('asi..xlsx', ['size']))
-
-print(file.update('test.txt', '\nGreat change', 3))
-# print(file.gettinfo('test.txt', ['size', 'format', 'preview']))
+print(file.update('test.txt', '\nGreat change again 3', 2))
+print(file.gettinfo('test.txt'))
 # print(file.delete('test.txt'))
